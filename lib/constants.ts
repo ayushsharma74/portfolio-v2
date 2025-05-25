0bb69dedaa7009303092d373c39ca24b9be6bc0a
Ayush Sharma <ayush.sharma74892@gmail.com>
2025-05-25 17:51:14 +0530

    twitter url changes

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,6 +1,6 @@
 export const SocialLinks = {
     github: "https://github.com/ayushsharma74",
-    x: "https://x.com/ayushon_twt",
+    x: "https://x.com/ayshtwt",
     linkedin: "https://www.linkedin.com/in/ayusshhhh/",
     discord: "https://discord.com/users/ayushsharma_93",
     instagram: "https://www.instagram.com/ayushsharma_93/"
